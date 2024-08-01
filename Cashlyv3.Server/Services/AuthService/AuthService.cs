b98c82b2cbe73c1974d68d676d727b20c7774453
eddyiiz <eddyxsayf@gmail.com>
2024-08-01 09:06:36 +0200

    Login & Register

--- Cashlyv3.Server/Services/AuthService/AuthService.cs
+++ Cashlyv3.Server/Services/AuthService/AuthService.cs
@@ -1,21 +1,82 @@
 ﻿
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
 namespace Cashlyv3.Server.Services.AuthService;
 
 public class AuthService : IAuthService
 {
     private readonly DataContext _context;
+    private readonly IConfiguration _config;
 
-    public AuthService(DataContext context)
+    public AuthService(DataContext context, IConfiguration config)
     {
         _context = context;
+        _config = config;
     }
-    public Task<ServiceResponse<int>> Register(User user, string password)
+    public async Task<ServiceResponse<int>> Register(User user, string password)
     {
-        throw new NotImplementedException();
+        var response = new ServiceResponse<int>();
+        try
+        {
+            if (await UserExists(user.Username))
+            {
+                response.Success = false;
+                response.Message = "Username Exists!";
+
+                return response;
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+
+            response.Data = user.Id;
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
     }
-    public Task<ServiceResponse<string>> Login(string username, string password)
+    public async Task<ServiceResponse<string>> Login(string username, string password)
     {
-        throw new NotImplementedException();
+        var response = new ServiceResponse<string>();
+        try
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username.ToLower()
+                    .Equals(username.ToLower()));
+
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "Not Found";
+            }
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                response.Success = false;
+                response.Message = "Wrong Password";
+            }
+            else
+            {
+                response.Data = CreateToken(user);
+            }
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
     }
 
     public Task<ServiceResponse<bool>> ChangePassword(int userId, string newPassword)
@@ -23,12 +84,66 @@ public class AuthService : IAuthService
         throw new NotImplementedException();
     }
 
-    public Task<bool> UserExists(string username)
-    {
-        throw new NotImplementedException();
-    }
     public Task<ServiceResponse<bool>> DeleteUser(int userId)
     {
         throw new NotImplementedException();
     }
+
+    public async Task<bool> UserExists(string username)
+    {
+        if (await _context.Users.AnyAsync(user => user.Username.ToLower()
+                 .Equals(username.ToLower())))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
+
+    private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512(passwordSalt))
+        {
+            var computeHash =
+                hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+            return computeHash.SequenceEqual(passwordHash);
+        }
+    }
+
+    private string CreateToken(User user)
+    {
+        List<Claim> claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Username)
+        };
+
+        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken
+            (
+              claims: claims,
+              expires: DateTime.Now.AddDays(1),
+              signingCredentials: creds
+            );
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return jwt;
+    }
+
 }
